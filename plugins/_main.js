72bf161a300999dbbe75504c4ed315e5ebd72a5c
GEEKMD099 <159744285+GEEKMD099@users.noreply.github.com>
2024-06-27 17:12:42 +0100

    Update _main.js

--- plugins/_main.js
+++ plugins/_main.js
@@ -489,7 +489,7 @@ smd({
     let {
       data: _0x44f98c
     } = await axios.get("https://api.github.com/repos/GEEKMD099/Richy-MD");
-    let _0x1c73f9 = ("\n Richy-Md github stat.\n\n  *❲❒❳ Stars:* " + (_0x44f98c?.stargazers_count || "120+") + " stars\n  *❲❒❳ Forks:* " + (_0x44f98c?.forks_count || "") + " forks\n  *❲❒❳ Authors:* Ednut\n  *❲❒❳ Created On:* " + (_0x44f98c?.created_at || "undefined") + "\n  *❲❒❳ Repo:* _https://github.com/GEEKMD099/Richy-MD_\n  *❲❒❳ Scan:* " + scan + "_" + (Config.caption ? "\n\n" + Config.caption : "")).trim();
+    let _0x1c73f9 = ("\n Richy-Md github stat.\n\n  *❲❒❳ Stars:* " + (_0x44f98c?.stargazers_count || "120+") + " stars\n  *❲❒❳ Forks:* " + (_0x44f98c?.forks_count || "") + " forks\n  *❲❒❳ Authors:* SLG\n  *❲❒❳ Created On:* " + (_0x44f98c?.created_at || "undefined") + "\n  *❲❒❳ Repo:* _https://github.com/GEEKMD099/Richy-MD_\n  *❲❒❳ Scan:* " + scan + "_" + (Config.caption ? "\n\n" + Config.caption : "")).trim();
     return await _0x45da98.sendUi(_0x45da98.jid, {
       caption: _0x1c73f9
     });
